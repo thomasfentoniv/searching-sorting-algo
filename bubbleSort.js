e657daaf8b1ebdd450d19af3c8df2a5aedfd2c1b
Thomas Fenton IV <tfentoniv@gmail.com>
2023-10-05 17:23:21 -0700

    Update bubbleSort.js

--- bubbleSort.js
+++ bubbleSort.js
@@ -4,9 +4,9 @@
  */
 function bubbleSort(compare, elements) {
   if (Array.isArray(elements)) {
+    let inOrder;
     do {
-      let inOrder = true;
-
+      inOrder = true;
       for (let i = 0; i < elements.length - 1; i++) {
         const leftElement = elements[i];
         const rightElement = elements[i + 1];
